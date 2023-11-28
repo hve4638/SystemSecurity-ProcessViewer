4bc1cfcae18e0f3bfcddba592f880b55b89fe491
clostrid <86879946+clostrid@users.noreply.github.com>
2023-11-28 17:32:15 +0900

    Update PC-19_deny_remotesupport.py

--- KISA_auto_check/PC-19_deny_remotesupport.py
+++ KISA_auto_check/PC-19_deny_remotesupport.py
@@ -21,21 +21,23 @@ def get_LMregistry_value(registry_path, value_name):
     except Exception as e:
         print("An error occurred: ", e)
         return None
-      
-# path of registry
-registry_path = r'SYSTEM\CurrentControlSet\Control\Terminal Server'
 
-# name of specific value
-value_name = 'fDenyTSConnections'
 
-Connection_value = get_LMregistry_value(registry_path, value_name)
-print(f"fDenyTSConnections 값: {Connection_value}")
-if Connection_value is not None:
-    if Connection_value == 1:
-        print("정상: 외부 데스크톱 연결이 차단되어 있습니다.")
-    elif Connection_value == 0:
-        print("취약점 발견: 외부 데스크톱 연결이 차단되어 있지 않습니다.")
+if __name__ == "__main__":
+    # path of registry
+    registry_path = r'SYSTEM\CurrentControlSet\Control\Terminal Server'
+
+    # name of specific value
+    value_name = 'fDenyTSConnections'
+
+    Connection_value = get_LMregistry_value(registry_path, value_name)
+    print(f"fDenyTSConnections 값: {Connection_value}")
+    if Connection_value is not None:
+        if Connection_value == 1:
+            print("정상: 외부 데스크톱 연결이 차단되어 있습니다.")
+        elif Connection_value == 0:
+            print("취약점 발견: 외부 데스크톱 연결이 차단되어 있지 않습니다.")
+        else:
+            print("취약점 발견: 예상치 못한 레지스트리 값")
     else:
-        print("취약점 발견: 예상치 못한 레지스트리 값")
-else:
-    print("취약점 발견: fDenyTSConnections 레지스트리 값을 읽을 수 없습니다.")
+        print("취약점 발견: fDenyTSConnections 레지스트리 값을 읽을 수 없습니다.")
