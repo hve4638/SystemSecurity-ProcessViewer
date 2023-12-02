cd3f583b12f9910d3c3c6f57ccaa46870a4d3423
kimojun <52404790+kimojun@users.noreply.github.com>
2023-12-02 15:17:12 +0900

    Create solve03.py

---
+++ security_check/solve03.py
@@ -0,0 +1,38 @@
+import os
+import winreg
+import subprocess
+
+"""
+PC-06에 대한 조치 함수
+type : link
+"""
+def solve_06():
+
+    # Run the specified command
+    os.system("start ms-settings:windowsupdate")
+
+"""
+PC-07에 대한 조치 함수
+type : link
+"""    
+def solve_07():
+    # Run the specified command
+    os.system("start ms-settings:windowsupdate")    
+    
+"""
+PC-08(한글업데이트)에 대한 조치 함수
+type : link
+"""
+def find_hancom_studio_path():
+    key_path = r"SOFTWARE\WOW6432Node\HNC\Shared\HncUpdate\HncUtils_2020\HancomStudio"
+    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path)
+    value, _ = winreg.QueryValueEx(key, "FilePath")
+    return value
+
+def solve_08_A():
+    key_path = r"SOFTWARE\WOW6432Node\HNC\Shared\HncUpdate\HncUtils_2020\HancomStudio"
+    key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path)
+    value, _ = winreg.QueryValueEx(key, "FilePath")
+
+    hancom_studio_path = value
+    subprocess.run([hancom_studio_path], check=True)       
