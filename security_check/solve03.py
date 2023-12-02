0cd9eb30ec0ed3f47f02468f30c00080297ba675
kimojun <52404790+kimojun@users.noreply.github.com>
2023-12-02 16:03:04 +0900

    Update solve03.py

--- security_check/solve03.py
+++ security_check/solve03.py
@@ -6,7 +6,7 @@ import subprocess
 PC-06에 대한 조치 함수
 type : link
 """
-def solve_pc06():
+def solve_pc06():  # HOT FIX 등 최신 보안패치 적용
     # Run the specified command
     os.system("start ms-settings:windowsupdate")
 
@@ -14,7 +14,7 @@ def solve_pc06():
 PC-07에 대한 조치 함수
 type : link
 """    
-def solve_pc07():
+def solve_pc07():  # 최신 서비스팩 적용
     # Run the specified command
     os.system("start ms-settings:windowsupdate")    
     
@@ -22,7 +22,7 @@ def solve_pc07():
 PC-08(한글업데이트)에 대한 조치 함수
 type : link
 """
-def solve_pc08_A():
+def solve_pc08_A():  # 응용프로그램에 대한 최신 패치 적용
     key_path = r"SOFTWARE\WOW6432Node\HNC\Shared\HncUpdate\HncUtils_2020\HancomStudio"
     key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path)
     value, _ = winreg.QueryValueEx(key, "FilePath")
